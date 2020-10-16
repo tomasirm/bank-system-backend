63c94914ea9a919122d713248033d2237a893163
Tomas <triquelme@creditu.com>
2020-10-16 18:56:54 -0300

    UPT: Se modifica config db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,25 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const databaseConfig: TypeOrmModuleOptions = {
+/*export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   port: 5432,
   host: 'localhost',
   database: 'bank',
   synchronize: true,
-  entities: ["dist/**/*.entity.js"],
+  entities: ["dist/!**!/!*.entity.js"],
   username: 'postgres',
   password: 'root',
   logging: false
-}
-
-/*
-type: 'postgres',
+}*/
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
   port: 5432,
   database: 'iirhlfrw',
   url: 'postgres://iirhlfrw:pfGnrOQck7Bu--7yF42r48WGHqdJ3LZf@salt.db.elephantsql.com:5432/iirhlfrw',
   synchronize: true,
-  entities: ["dist/!**!/!*.entity.js"],
+  entities: ['dist/**/*.entity.js'],
   username: 'iirhlfrw',
   password: 'pfGnrOQck7Bu--7yF42r48WGHqdJ3LZf',
-  logging: false
-*/
+  logging: false,
+};
