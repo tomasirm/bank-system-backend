f6bd628c6a1aa2f0fdf5a6b9492daf7a1ccd3bb2
Tomas <triquelme@creditu.com>
2020-10-19 11:34:00 -0300

    UPT: variables nulleables

--- src/modules/customer/customer.entity.ts
+++ src/modules/customer/customer.entity.ts
@@ -4,11 +4,11 @@ import { TransactionEntity } from '../transaction/transaction.entity';
 
 @Entity({name: 'customer'})
 export class CustomerEntity extends GenericEntity{
-  @Column({ type: 'varchar', length: 20, nullable: false })
+  @Column({ type: 'varchar', length: 20, nullable: true })
   dni: string;
-  @Column({ type: 'varchar', length: 50, nullable: false })
+  @Column({ type: 'varchar', length: 50, nullable: true })
   names: string;
-  @Column({ type: 'varchar', length: 20, nullable: false })
+  @Column({ type: 'varchar', length: 20, nullable: true })
   surnames: string;
   @Column({ type: 'varchar', length: 50, nullable: true })
   email: string;
