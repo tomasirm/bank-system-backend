ad9d4b0be68e9a2dba57d9479743fd149bbc58aa
Tomas <triquelme@creditu.com>
2020-10-19 11:31:59 -0300

    UPT: variables nulleables

--- src/modules/transaction-types/transaction-types.entity.ts
+++ src/modules/transaction-types/transaction-types.entity.ts
@@ -5,13 +5,13 @@ import { TransactionEntity } from '../transaction/transaction.entity';
 @Entity({name: 'transaction_types'})
 export class TransactionTypesEntity extends GenericEntity{
 
-  @Column({ type: 'varchar', nullable: false, length: 50})
+  @Column({ type: 'varchar', nullable: true, length: 50})
   name: string;
 
   @Column({ type: 'varchar', nullable: true, length: 50})
   label: string;
 
-  @Column({ type: 'varchar', nullable: false, length: 100})
+  @Column({ type: 'varchar', nullable: true, length: 100})
   description: string;
 
   @OneToMany(type => TransactionEntity, transaction => transaction.transactionType) // note: we will create author property in the Photo class below
